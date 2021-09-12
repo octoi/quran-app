481d9e0c4e9029439edfc931042a5ef0e3d384ac
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-12 15:19:16 +0530

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quran/model/suraths.dart';
 import 'package:quran/screens/home_screen.dart';
 
 void main() {
@@ -10,9 +12,14 @@ class QuranApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: Suraths()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
