64e019a75f795d496d6b139c925a1c19628982dc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-12 14:48:36 +0530

    api

---
+++ lib/api/api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quran/model/suraths.dart';
+
+String originUrl = 'https://api.quran.com/api/v4';
+
+Future<List<Surah>> requestAllSurahs() async {
+  List<Surah> _surahs = [];
+
+  Uri url = Uri.parse('$originUrl/chapters');
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    var decodedData = jsonDecode(response.body);
+    decodedData['chapters'].forEach((surah) {
+      var newSurah = Surah(
+        id: surah['id'],
+        surah: surah['name_arabic'],
+        simpleName: surah['name_simple'],
+        pages: '1-10',
+        origin: surah['revelation_place'],
+        verses: surah['verses_count'],
+      );
+      _surahs.add(newSurah);
+    });
+  }
+
+  return _surahs;
+}
