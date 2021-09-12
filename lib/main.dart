9bb2a040a0e04b4dbebbbf222d04a51083578429
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-12 10:06:43 +0530

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:quran/screens/home_screen.dart';
 
 void main() {
-  runApp(Quran());
+  runApp(QuranApp());
 }
 
-class Quran extends StatelessWidget {
-  const Quran({Key? key}) : super(key: key);
+class QuranApp extends StatelessWidget {
+  const QuranApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(),
     );
   }
 }
--- lib/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color appPrimaryColor = Color(0xFFFCFCFC);
-const Color appSecondaryColor = Color(0xFF17161A);
-const Color appAccentColor = Color(0xFF52CAAD);
-const Color appTextColor = Color(0xFF7D7C80);
