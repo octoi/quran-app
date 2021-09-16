482ed0baa47f570b375786c3e3e39a015c17aaa0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-16 12:37:40 +0530

    ...

--- lib/screens/surah_screen.dart
+++ lib/screens/surah_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:quran/model/suraths.dart';
 import 'package:quran/utils/constants.dart';
 import 'package:quran/widgets/home_screen/like_button.dart';
@@ -35,6 +36,34 @@ class SurahScreen extends StatelessWidget {
         actions: [
           LikeButton(surah: surah),
         ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 20.0,
+        ),
+        child: FutureBuilder(
+          future: Provider.of<Suraths>(
+            context,
+            listen: false,
+          ).fetchAndSetSurayAyaths(surah),
+          builder: (context, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading ...');
+            }
+            return Consumer<Suraths>(
+              builder: (BuildContext context, suraths, _) {
+                return Text(
+                  suraths.getOneSurah(surah.id).verses,
+                  style: GoogleFonts.nunito(
+                    color: appDark,
+                    fontSize: 20.0,
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
